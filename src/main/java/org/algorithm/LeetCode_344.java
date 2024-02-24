3dce5421211cfcdeeaab3bec5e24768ac4fd51b7
JayeveYang <Jayeveyang@gmail.com>
2024-02-24 22:06:23 +1100

    反转字符串

--- src/main/java/org/algorithm/LeetCode_344.java
+++ src/main/java/org/algorithm/LeetCode_344.java
@@ -2,16 +2,16 @@ package org.algorithm;
 
 public class LeetCode_344 {
     /*
-    * 题目
-    * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
-    * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
-    * */
+     * 题目
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     * */
     public void reverseString(char[] s) {
-        for(int i=0; i<s.length/2; i++){
+        for (int i = 0; i < s.length / 2; i++) {
             char temp;
-            temp=s[i];
-            s[i]=s[s.length-1-i];
-            s[s.length-1-i]=temp;
+            temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
         }
     }
 }
