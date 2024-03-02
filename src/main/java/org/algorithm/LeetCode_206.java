6bc769361524a2ce3a269cfaa6bd41a4407f9a8f
JayeveYang <Jayeveyang@gmail.com>
2024-03-02 20:16:30 +1100

    反转链表

---
+++ src/main/java/org/algorithm/LeetCode_206.java
@@ -0,0 +1,39 @@
+package org.algorithm;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class LeetCode_206 {
+    /*
+    * 题目
+    * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+    *
+    * 思路
+    * 双指针
+    * */
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode nex = null;
+        while (cur != null) {
+            nex = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nex;
+        }
+        return pre;
+    }
+}
